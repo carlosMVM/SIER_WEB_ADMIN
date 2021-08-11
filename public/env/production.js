e0222c7c306ba1fcbe31189c4f48ea9df16a912b
carlosMVM <87099199+carlosMVM@users.noreply.github.com>
2021-08-11 15:12:11 -0500

    Update production.js

--- public/env/production.js
+++ public/env/production.js
@@ -1,5 +1,5 @@
 window.envVariables = {
-	apiUrl: "https://appimotion-backend-prd.appimotion.com/es/api",
+	//apiUrl: "https://appimotion-backend-prd.appimotion.com/es/api",
 	googleMapsKey: "AIzaSyCJHkOy94hwMLZJksqsnkQ06AvWkCd6ea0",
-	//apiUrl: "https://appimotion-backend-dev.appimotion.com/es/api"
+	apiUrl: "https://appimotion-backend-dev.appimotion.com/es/api"
 };
